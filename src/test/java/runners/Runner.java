28f7367754886c1a7ac77f4f9a6f6dadd6352785
Vlad <vladmonqa35@gmail.com>
2020-02-12 23:04:59 -0600

    add Json Report to Cucumber framework

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -8,18 +8,21 @@ import org.junit.runner.RunWith;
 // thats how cucumber works
 
 @CucumberOptions(
-        plugin = "html:target/cucumber",
 
 // this plugin just for generate reports of all the test
 // we simply create folder cucumber in target folder
-        features = "src/test/resources/features",
+// saves our results of tests in cucumber report, and json report
+        plugin = {"html:target/cucumber", "json:target/report.json"},
 
 // glue our features(under resources) with our steps folder
+        features = "src/test/resources/features",
+
         glue = "steps",
 // to run specific feature we use tags.
 // we can run multiple features like tags="@Amazon, @google"
 // or just one tags = "@Amazon"
         tags = "@datatables3",
+
 // dryRun its tag that avoid execution (dont runn browser) checket if all method generated
         dryRun = false
 )
