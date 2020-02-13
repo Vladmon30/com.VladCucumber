944f442aaad8ce56529d257f06abbc9b490381f9
Vlad <vladmonqa35@gmail.com>
2020-02-13 11:03:48 -0600

    update 2/12/20

--- src/test/java/steps/Test_steps.java
+++ src/test/java/steps/Test_steps.java
@@ -4,7 +4,7 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.junit.Assert;
 
-public class Test_steps {fdsf
+public class Test_steps {
     @When("user goes to google application")
     public void user_goes_to_google_application() {
         System.out.println("selenium is going to google.com");
@@ -15,13 +15,13 @@ public class Test_steps {fdsf
         System.out.println("user searching for apple");
     }
 
-    @Then("user should see apple related results")fsf
+    @Then("user should see apple related results")
     public void user_should_see_apple_related_results() {
-        1System.out.println("user checking the result");
+        System.out.println("user checking the result");
        // Assert.fail("failing for a reason");
     }
 
-    @Then("user tests something")ffsf
+    @Then("user tests something")
     public void user_tests_something() {
         System.out.println("user doing something");
     }
